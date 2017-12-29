d84597c268206949e50a4e28241d3dca2e45e343
Markus Schiller <markus.schiller@appundmensch.de>
2017-12-29 17:57:47 +0100

    Renamed and fixed styles

--- src/Sequential.py
+++ src/Sequential.py
@@ -10,8 +10,8 @@ numpy.random.seed(seed)
 # load pima indians dataset
 dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
+X = dataset[:, 0:8]
+Y = dataset[:, 8]
 # create model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu', kernel_initializer='uniform'))
@@ -20,7 +20,7 @@ model.add(Dense(1, activation='sigmoid', kernel_initializer='uniform'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
-model.fit(X, Y, epochs=150, batch_size=10,  verbose=2)
+model.fit(X, Y, epochs=150, batch_size=10, verbose=2)
 # Evaluate the model
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
