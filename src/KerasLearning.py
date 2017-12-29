1aa8635416a08afebee8076664cd791b7d0f16ad
Markus Schiller <markus.schiller@appundmensch.de>
2017-12-29 17:49:31 +0100

    Removed Warnings

--- src/KerasLearning.py
+++ src/KerasLearning.py
@@ -14,13 +14,16 @@ X = dataset[:,0:8]
 Y = dataset[:,8]
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
+model.add(Dense(12, input_dim=8, activation='relu', kernel_initializer='uniform'))
+model.add(Dense(8, activation='relu', kernel_initializer='uniform'))
+model.add(Dense(1, activation='sigmoid', kernel_initializer='uniform'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
 model.fit(X, Y, epochs=150, batch_size=10,  verbose=2)
+# Evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 # calculate predictions
 predictions = model.predict(X)
 # round predictions
